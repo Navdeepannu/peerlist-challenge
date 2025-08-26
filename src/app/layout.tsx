bd4319235ae06628184b9f9f033dc3e5a234cdfb
Navdeep Singh <84785739+Navdeepannu@users.noreply.github.com>
2025-08-26 11:57:44 -0400

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -54,11 +55,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        <main>
-          <Navbar />
-          {children}
-        </main>
+      <body
+        className={`${inter.className} antialiased min-h-screen flex flex-col`}
+      >
+        <Navbar />
+        <main className="flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TextHoverEffect } from "./text-hover-effect";
+
+const Footer = () => {
+  return (
+    <div className="dark:bg-black bg-white">
+      <TextHoverEffect text="NAVDEEP" />
+    </div>
+  );
+};
+
+export default Footer;
